3b7c23decbe2d6d7a98e95578dbfb63409e8b4d3
ittp <61617660+ittp@users.noreply.github.com>
2023-06-14 22:42:24 +0300

    1 file modified

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const zabbix = require("zabbix-client")
+const axios = require("axios")
+const dns = require("dns")
+
+let hosts = ["5.17.2.147", "188.134.65.67"]
 
 export default (req, res) => {
   // Open Chrome DevTools to step through the debugger!
   // debugger;
+
 
   
   res.status(200).json({ name: 'Hello, world!' });
